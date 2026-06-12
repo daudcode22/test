8e5cfec4e0493a92daf156c4220684c1781676f1
Rizqi Daud Abdillah <daudpersonal22@gmail.com>
2026-06-12 22:48:23 +0700

    add integer input to var.c

--- var/var.c
+++ var/var.c
@@ -8,12 +8,15 @@ int main() {
 
 	float Number;
 	float NumberTwo;
-
+	int TestNumber;
 	printf("masukan floating number pertama : ");
 	scanf("%f",&Number);
 	printf("masukan floating number kedua : ");
 	scanf("%f",&NumberTwo);
+	printf("\n masukan bilangan bulat : ");
+	scanf("%d",&TestNumber);
 	Kali(&Number,&NumberTwo);
+	printf("\n %d",TestNumber * NumberTwo);
 
 
 }
